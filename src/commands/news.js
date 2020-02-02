34966552c4627d1ef6db384cd315be0beee17927
ErKiran <kiruu1238@gmail.com>
2020-02-02 15:23:05 +0545

    Trending News Added

--- src/commands/news.js
+++ src/commands/news.js
@@ -1,16 +1,38 @@
 const program = require('commander');
 const chalk = require('chalk');
-const { getBreakingNews } = require('../controllers/news')
+const { getBreakingNews, getTrendingNews } = require('../controllers/news')
 
 program
   .command('breakingNews')
-  .description(`${chalk.red('🔥🌋 Breaking News 🔥')}`)
+  .alias('brk')
+  .description(`${chalk.red('🔥🔥 Breaking News 🔥🔥')}`)
   .action(async function BreakingNews() {
-    const news = await getBreakingNews();
-    console.log(chalk.yellowBright(`The Breaking News for ${news.todaysDate} are`))
-    news.news.map(i => {
-      console.log(chalk.redBright(i.title))
-      console.log(chalk.magentaBright(`Read More Here ${i.link}`))
-    })
-    process.exit(1)
+    try {
+      const news = await getBreakingNews();
+      console.log(chalk.yellowBright(`The Breaking News for ${new Date()} are`))
+      console.log('')
+      news.map(i => {
+        console.log(chalk.redBright(`🔥🔥 ${i.title}`))
+        console.log(chalk.magentaBright(`Read More👉👉 ${i.link}`))
+        console.log('')
+      })
+      process.exit(1)
+    }
+    catch (err) {
+      throw new Error(`Can't process breakingNews Command ${err}`)
+    }
+  })
+
+program
+  .command('trendingNews')
+  .alias('trd')
+  .description(`${chalk.green('❤️❤️ Popular News ❤️❤️')}`)
+  .action(async function popularNews() {
+    try {
+      const news = await getTrendingNews()
+    }
+    catch (err) {
+      throw new Error(`Can't process TrendingNews Command ${err}`)
+    }
+
   })
--- src/controllers/news.js
+++ src/controllers/news.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const LINK_URL = 'http://annapurnapost.com/news/'
 const { BASE_URL } = process.env
 
 async function getBreakingNews() {
@@ -8,21 +9,35 @@ async function getBreakingNews() {
     await Promise.all(specialNews.data.data.map(i => {
       news.push({
         title: i.title,
-        link: `http://annapurnapost.com/news/${i.id}`
+        link: `${LINK_URL}${i.id}`
       })
     }))
-    const todaysDate = specialNews.data.todaysDate;
-    return {
-      news,
-      todaysDate
-    }
+    return news
   }
   catch (err) {
     throw new Error(`Can't fetch Latest Breaking news ${err}`)
   }
 }
 
+async function getTrendingNews() {
+  try {
+    const trendingNews = await axios.get(`http://bg.annapurnapost.com/api/trending/news`);
+    const trends = [];
+    await Promise.all(trendingNews.data.data.map(i => {
+      trends.push({
+        title: i.title,
+        link: `${LINK_URL}${i.id}`,
+        author: i.author.name
+      })
+    }))
+    return trends
+  }
+  catch (err) {
+    throw new Error(`Can't fetch Trending News ${err}`)
+  }
+}
 
 module.exports = {
-  getBreakingNews
+  getBreakingNews,
+  getTrendingNews
 }
